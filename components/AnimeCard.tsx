d638e5aac83ed696b09f4090b6f79ec71e8ce1b5
Dawit Diriba <58243647+DAdawit@users.noreply.github.com>
2023-12-06 13:43:18 +0300

    animation change

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -30,7 +30,7 @@ function AnimeCard({ anime, index }: Prop) {
       initial="hidden"
       animate="visible"
       transition={{
-        delay: index * 0.15,
+        delay: index * 0.25,
         ease: "easeInOut",
         duration: 0.5,
       }}
